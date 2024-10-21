1b726c1d872f465630e0c54ec7b716e8b83422eb
Tim Niederer <tim.niederer@weichwarenprojekt.de>
2024-10-21 14:14:44 +0200

    feat: add a scroll down indicator

---
+++ app/components/scroll-down/scroll-down.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styles from "./scroll-down.module.scss";
+
+/**
+ * The scroll down indicator
+ */
+export const ScrollDown: React.FC = () => {
+    return <div className={styles.scrollDown}></div>;
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.scss";
 import { Disco } from "@/app/disco/disco";
+import { ScrollDown } from "@/app/components/scroll-down/scroll-down";
 
 /**
  * The home page
@@ -9,7 +10,15 @@ export default function Home() {
         <div className={styles.page}>
             <main className={styles.main}>
                 <Disco />
-                <h1 className={styles.title}>DiscoDaddy</h1>
+                <section className={styles.intro}>
+                    <h1>DiscoDaddy</h1>
+                    <ScrollDown />
+                </section>
+                <section className={styles.about}>
+                    <h2>Daddy?</h2>
+                    <h2>Who the fuck is Daddy?</h2>
+                    <ScrollDown />
+                </section>
             </main>
             <footer className={styles.footer}>Test</footer>
         </div>
