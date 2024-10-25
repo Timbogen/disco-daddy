b87f7fa138af3e788960f7081c3c0f9eace67f67
Tim Niederer <tim.niederer@weichwarenprojekt.de>
2024-10-25 09:41:09 +0200

    feat: calculate age dynamically

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import styles from "./page.module.scss";
 import clsx from "clsx";
 import { Gallery } from "@/app/components/gallery/gallery";
 import { useProgress } from "@react-three/drei";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Contact } from "@/app/components/contact/contact";
 import dynamic from "next/dynamic";
 
@@ -27,6 +27,15 @@ export default function Home() {
     useEffect(() => {
         if (progress >= 100) setShowBorders(true);
     }, [progress]);
+
+    /** Calculate the age */
+    const age = useMemo(() => {
+        const dateOfBirth = new Date("10/25/1964");
+        const monthDifference = Date.now() - dateOfBirth.getTime();
+        const ageDate = new Date(monthDifference);
+        const year = ageDate.getUTCFullYear();
+        return Math.abs(year - 1970);
+    }, []);
 
     return (
         <main className={clsx("page", showBorders ? "" : styles.initialPage)}>
@@ -57,7 +66,7 @@ export default function Home() {
                             >
                                 <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" />
                             </svg>
-                            Georg Schmid, 60, stolzer Vater (👧🏽👦🏼)
+                            Georg Schmid, {age}, stolzer Vater (👧🏽👦🏼)
                         </div>
                         <div className={styles.info}>
                             <svg
